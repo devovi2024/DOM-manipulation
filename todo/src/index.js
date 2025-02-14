be117e91e965e24a71648ed645d600958f20ca92
devovi2024 <arfanhosenovi@gmail.com>
2025-02-15 02:49:44 +0600

    categories fetch

--- todo/src/index.js
+++ todo/src/index.js
@@ -7,16 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
         {name: "Code Think", task: 5, color:"Meeting"},
     ];
 
-    const categoryContainer = document.getElementById('categories')
+    const tasks = [];
+    let selectedCategory = null;
 
-    function renderCategories(){
+    const categoryContainer = document.getElementById('categories');
+    const taskContainer = document.getElementById("taskList");
+    const addTaskButton = document.getElementById("assTaskBtn");
+
+
+    function renderCategories() {
         categoryContainer.innerHTML = "";
         categories.forEach(category => {
-        const li = document.createElement('div');
-        li.textContent = category.name;
-        li.className = "p-3 border rounded shadow-md"
-        categoryContainer.appendChild(li)
-
+            const div = document.createElement("div");
+            div.className = `${categories.color} p-4 rounded-xl text-center cursor-pointer`;
+            div.innerHTML = `
+                <p class="text-sm font-semibold">${category.name}</p>
+            `
+            categoryContainer.appendChild(div)
         })
     }
     renderCategories()
