8b406ba23b06c3c4a6335ac5d43e0a2078ffdf0c
devovi2024 <arfanhosenovi@gmail.com>
2025-02-28 01:30:22 +0600

    toggle

---
+++ DOM/toogle/script.js
@@ -0,0 +1,24 @@
+// Iron Man Helmet Toggle
+function toggleHelmet() {
+    document.getElementById("helmet").classList.toggle("open-mode");
+}
+
+// Lightsaber Toggle
+function toggleLightsaber() {
+    document.getElementById("lightsaber").classList.toggle("activated");
+}
+
+// Invisibility Cloak Toggle
+function toggleCloak() {
+    document.getElementById("cloak").classList.toggle("invisible");
+}
+
+// AI-HUD Display Toggle
+function toggleHUD() {
+    document.getElementById("aiHUD").classList.toggle("display-on");
+}
+
+// Cybernetic Arm Toggle
+function toggleCyberArm() {
+    document.getElementById("cyberArm").classList.toggle("power-mode");
+}
